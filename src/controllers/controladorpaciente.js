e9687d95df94dce1f6f4406cbbc99c61d05908a9
Lina <mi_marce1@hotmail.com>
2024-10-23 16:10:51 -0500

    se controlador paciente

---
+++ src/controllers/controladorpaciente.js
@@ -0,0 +1,16 @@
+//OBJETIVO: Capturar los datos de un formulario
+
+//1. Por cada input, select, textarea del formulario se crea una variable
+
+//2. Por cada variable debo utilizar el DOM para asociar el HTML con js
+
+let nombrePaciente=document.getElementById("nombrepaciente")
+let correoPaciente=document.getElementById("correopaciente")
+let telefonoPaciente=document.getElementById("telefonopaciente")
+let fechaNacimientoPaciente=document.getElementById("nacimientopaciente")
+let ciudadPaciente=document.getElementById("ciudadpaciente")
+let polizaPaciente=document.getElementById("polizapaciente")
+let ipsPaciente=document.getElementById("ipspaciente")
+let grupoIngresoPaciente=document.getElementById("ingresopaciente")
+let fechaAfiliacionPaciente=document.getElementById("afiliacionpaciente")
+
